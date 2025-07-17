4abaae7fcd55ee0fb841306aaa75d978639a888b
lavboj <58738866+lavboj@users.noreply.github.com>
2025-07-17 21:00:14 +0200

    Refactoring of giftFetcher.py module

--- service/giftFetcher.py
+++ service/giftFetcher.py
@@ -1,12 +1,12 @@
-from custom_tl.payments import GetStarGiftsRequest
+from telethon import functions
 
 def gift_parameters(gift) -> dict:
     return {
         "id": getattr(gift, "id", None),
-        "price": getattr(gift, "stars", 0),
+        "price": getattr(gift, "stars", 0) or 0,
         "total": getattr(gift, "availability_total", 0),
-        "left": getattr(gift, "sold_out", False),
-        "availability": getattr(gift, "availability_remains", 0),
+        "sold_out": getattr(gift, "sold_out", False),
+        "availability": getattr(gift, "availability_remains", 0) or 0,
         "limited": getattr(gift, "limited", False)
     }
 
@@ -18,24 +18,23 @@ async def gift_filter(
         min_price,
         max_price,
         min_supply,
-        max_supply,
 )-> list:
     
-    api_gifts = await bot(GetStarGiftsRequest(hash=0))
+    api_gifts = await bot(functions.payments.GetStarGiftsRequest(hash=0))
 
     filtered_gifts = []
     for gift in api_gifts.gifts:
         params = gift_parameters(gift)
 
         price = params.get("price")
-        sold_out = params.get("left")
+        sold_out = params.get("sold_out")
         is_limited = params.get("limited")
         supply = params.get("availability")
 
         if not is_limited or sold_out:
             continue
 
-        if min_price <= price <= max_price and min_supply <= supply <= max_supply:
+        if min_price <= price <= max_price and min_supply <= supply:
             filtered_gifts.append(gift)
 
     return filtered_gifts
