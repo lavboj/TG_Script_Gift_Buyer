361425b6fc3d753e56bc461f9077129798afa71a
lavboj <58738866+lavboj@users.noreply.github.com>
2025-07-23 21:20:00 +0200

    Refactoring balance.py module

--- service/balance.py
+++ service/balance.py
@@ -1,20 +1,26 @@
-import logging
 from telethon import functions, errors
-import asyncio
 
+from utils.retry import make_retry_wrapper
+
+@make_retry_wrapper(
+    max_retries=3,
+    delay=1,
+    retry_exceptions=(
+        errors.FloodWaitError,
+        errors.ServerError,
+        errors.TimeoutError,
+        ConnectionError,
+        errors.RPCError
+    )
+)
 async def get_star_balance(client, peer) -> float:
-    try:
-        result = await client(functions.payments.GetStarsTransactionsRequest(
-            peer=peer,
-            offset='',
-            limit=1
-        ))
-        stars = result.balance    
-        return stars.amount + stars.nanos / 1e9
-    
-    except (errors.RPCError, ConnectionError, asyncio.TimeoutError) as e:
-        logging.error(f"Failed to get star balance: {e}")
-        raise
+    result = await client(functions.payments.GetStarsTransactionsRequest(
+        peer=peer,
+        offset='',
+        limit=1
+    ))
+    stars = result.balance    
+    return stars.amount + stars.nanos / 1e9
 
 # Этот модуль отвечает за получение баланса звезд в Telegram через Telethon.
 # Функция `get_star_balance` принимает объект клиента и возвращает баланс звезд
