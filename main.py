fabe5b4a138573ae9a0bbc9599702167f8920648
lavboj <58738866+lavboj@users.noreply.github.com>
2025-07-26 22:11:10 +0200

    Error strings translated from rus to eng

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from dotenv import load_dotenv
 from service.balance import get_star_balance
 from service.buy import buy_gift
 from service import gift_fetcher, auth
+from utils import log_config
+
+log_config.setup_logging(level=logging.DEBUG) 
 
 async def start_script(api_id, api_hash):
     
@@ -23,10 +26,10 @@ async def start_script(api_id, api_hash):
         channel_peer = await client.get_input_entity(CHANNEL_NAME)
         peer = await auth_client.get_input_peer()
     except RuntimeError as e:
-        logging.critical(f"Ошибка при подключении к Telegram: {e}")
+        logging.critical(f"Error connecting to Telegram: {e}")
         sys.exit(1)
     except Exception as e:
-        logging.critical(f"Непредвиденная ошибка в main: {e}")
+        logging.critical(f"Unexpected error in main: {e}")
         sys.exit(1)
     
     print("Сканируем подарки...")
@@ -35,15 +38,16 @@ async def start_script(api_id, api_hash):
     while True:
         # Получение списка подарков по заданным параметрам
         if not client.is_connected():
-            break
+            logging.critical("Reconnection failed")
+            sys.exit(1)
         try:
             gift_list = await gift_fetcher.gift_filter(client, 200, 2000, 250000)
             balance = await get_star_balance(client, peer)
         except RuntimeError as e:
-            logging.error(f"Ошибка с исчерпанными попытками: {e}")
+            logging.error(f"Error: {e}")
             continue
         except Exception as e:
-            logging.error(f"Непредвиденная ошибка: {e}")
+            logging.error(f"Unexpected error: {e}")
             continue
 
         # Если есть подарки, начинаем их покупку опираясь на текущий баланс
@@ -60,10 +64,10 @@ async def start_script(api_id, api_hash):
                     balance = await get_star_balance(client, peer)
                     await asyncio.sleep(0.5)
                 except RuntimeError as e:
-                    logging.error(f"Ошибка с исчерпанными попытками: {e}")
+                    logging.error(f"Error: {e}")
                     continue
                 except Exception as e:
-                    logging.error(f"Непредвиденная ошибка: {e}")
+                    logging.error(f"Unexpected error: {e}")
                     continue
 
         await asyncio.sleep(0.5)
@@ -80,7 +84,7 @@ if __name__ == "__main__":
     CHANNEL_NAME = os.getenv('CHANNEL_NAME')
 
     if not API_HASH or not API_ID or not CHANNEL_NAME:
-        logging.critical("API_ID или API_HASH или CHANNEL_NAME не заданы в .env")
+        logging.critical("API_ID, API_HASH, or CHANNEL_NAME is not set in the .env file")
         sys.exit(1)
 
     asyncio.run(start_script(API_ID, API_HASH))
